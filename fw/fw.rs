80b51aa1682a203942fdc8d04e93a932bc67711e
Jim Heald <james.r.heald@gmail.com>
2017-12-11 11:56:16 -0500

    Needed to use iter() instead of into_iter(), as into_iter() consumes the vector

--- fw/fw.rs
+++ fw/fw.rs
@@ -8,12 +8,14 @@ fn main() {
     
     /* Iterate over each argument, making tuples of (index, arg). Try casting the
      * result into something, and if it is something set the index as appropriate */
-    // FIXME: This scope is taking ownership of args, so I can't use it later
-    if let Some(tup) = args.into_iter().enumerate().find(|ref arg| arg.1 == "-n") {
+    /* NOTE: Using args.into_iter() would consume self (i.e. args) because into_iter
+     * takes self as a parameter, whereas iter() takes &self as a parameter */
+    if let Some(tup) = args.iter().enumerate().find(|ref arg| arg.1 == "-n") {
         num_index = tup.0;
     };
 
     if num_index != 0 {
+    // TODO: Make sure next argument after flag is valid number
         println!("Found n flag at index {}. Next arg is {}", num_index, args[num_index + 1]);
     }
 }
