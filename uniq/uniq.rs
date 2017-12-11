185a1c134581db32dfb894585d2034908e11cee4
Jim Heald <james.r.heald@gmail.com>
2017-12-10 18:15:34 -0800

    Some clarifing comments, better naming for line string variable

--- uniq/uniq.rs
+++ uniq/uniq.rs
@@ -1,22 +1,31 @@
 use std::io;
+/* Since the StdinLock returned by stdin.lock() implements the BufRead trait
+ * we must import it in order to use its functions (e.g. .lines() ) */ 
 use std::io::BufRead;
 
 fn main() {
     let mut last_line = String::new();
 
     let stdin = io::stdin();
+    /* Lock stdin to get a locked reference to stdin. I think this is a mutex,
+     * and it prevents stdin from being read unexpectedly in other places */
     let handle = stdin.lock();
 
+    /* Lines will read in a line one by line, and strip away the trailing newline.
+     * It will return a Result, for which we can match to a successful result or
+     * an error. */
     for line in handle.lines() {
-        let line = match line {
+        let line_str = match line {
             Ok(line) => line,
             Err(err) => panic!("failed to read line: {}", err)
         };
 
-        if line != last_line {
-            println!("{}", line);
+        // Strings can be directly compared
+        if line_str != last_line {
+            println!("{}", line_str);
         }
 
-        last_line = line;
+        // Strings can also be directly assigned
+        last_line = line_str;
     }
 }
