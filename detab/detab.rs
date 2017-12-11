ca442cd66dcc4b5481a5291132fc1397c69f1404
Jim Heald <james.r.heald@gmail.com>
2017-12-11 09:49:15 -0500

    Instead of calculating the number of spaces and calling print!(" ") on each, just borrow a slice of a static full tab

--- detab/detab.rs
+++ detab/detab.rs
@@ -3,24 +3,29 @@
 
 use std::io;
 use std::io::BufRead;
+use std::io::Write;
+
+// We know this is the max a tab will be, so let's slice it!
+const TAB: [u8; 8] = *b"        ";
 
 fn main() {
     let stdin = io::stdin();
-    let handle = stdin.lock();
 
-    for line in handle.lines() {
+    let stdout_raw = io::stdout();
+    let mut stdout = stdout_raw.lock();
+
+    for line in stdin.lock().lines() {
         let line_str = match line {
             Ok(line) => line,
             Err(err) => panic!("failed to read line: {}", err)
         };
+
         let mut num = 0;
 
         for chr in line_str.chars() {
             if chr == '\t' {
-                let num_iters = 8 - (num % 8);
-                for _ in 0..num_iters {
-                    print!(" ");
-                }
+                // Borrow a slice of TAB to get the correct number of spaces
+                let _ = stdout.write_all(&TAB[..8 - (num % 8)]);
                 num = 0;
             } 
             else {
