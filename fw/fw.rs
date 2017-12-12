4303e09e4fdfb8e31068413f062637741359a412
Jim Heald <james.r.heald@gmail.com>
2017-12-11 19:32:52 -0500

    Gracefully handle command line, get a list of files at the end

--- fw/fw.rs
+++ fw/fw.rs
@@ -1,21 +1,69 @@
 use std::env;
+use std::process;
 
 fn main() {
-    let mut num_index = 0;
+    // Temporary mutable variables for parsing command line
+    let mut _num_index = 0;
+    let mut _num_words = 10;
 
     // Make the command line arguments into a vector
-    let args: Vec<_> = env::args().collect();
+    let mut args: Vec<_> = env::args().collect();
+    let mut prog_name = String::new();
     
     /* Iterate over each argument, making tuples of (index, arg). Try casting the
      * result into something, and if it is something set the index as appropriate */
     /* NOTE: Using args.into_iter() would consume self (i.e. args) because into_iter
      * takes self as a parameter, whereas iter() takes &self as a parameter */
     if let Some(tup) = args.iter().enumerate().find(|ref arg| arg.1 == "-n") {
-        num_index = tup.0;
+        _num_index = tup.0;
     };
 
+    // We've figured out where the "-n" flag is, it's a constant now
+    let num_index = _num_index;
     if num_index != 0 {
-    // TODO: Make sure next argument after flag is valid number
-        println!("Found n flag at index {}. Next arg is {}", num_index, args[num_index + 1]);
+        if num_index + 1 >= args.len() {
+            print_usage(&args[0]);
+        }
+
+        // This pattern matching sees if the result is an Ok or Err, and binds
+        // the inner element to the corresponding name (either n or e)
+        match args[num_index+1].parse::<i32>() {
+            Ok(n) => { 
+                _num_words = n;
+
+                // Done with that data, let's make args only contain the filenames
+                // we need to read now
+                // NOTE: Need to remove them in reverse, since indexing changes
+                args.remove(num_index+1);
+                args.remove(num_index);
+                prog_name = args.remove(0); // Don't need the program name anymore
+            },
+            Err(e) => {
+                eprintln!("{}", e);
+                // Note that we can't give an element of an array to a function
+                // (i.e. it can't take ownership of that element) so we must pass
+                // it by reference
+                print_usage(&args[0]);
+            }
+        }
     }
+    else {
+        prog_name = args.remove(0);
+    }
+
+    if args.len() < 1 {
+        print_usage(&prog_name);
+    }
+
+    // Number of words finalized, no more modifying
+    let num_words = _num_words;
+
+    println!("Ready to find the most frequent {} words in the file", num_words);
+    println!("Going to read the following files: {:?}", args);
 }
+
+fn print_usage(name: &String) {
+    eprintln!("USAGE: {} [-n] [FILE]...", name);
+    process::exit(-1);
+}
+
