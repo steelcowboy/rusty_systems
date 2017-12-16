b94da92d71c73205c5163891ae7d4278f394e26f
Jim Heald <james.r.heald@gmail.com>
2017-12-15 22:33:08 -0500

    Some starting code, need to write utilizing generics though and figure out how to factor in linked list

--- htable/src/tree.rs
+++ htable/src/tree.rs
@@ -1,3 +1,65 @@
-pub fn test() {
-    println!("Tree, reporting in");
+use std::cmp;
+use std::cmp::Ordering;
+
+// This will set the bools to false and the nums to 0
+#[derive(Default)]
+pub struct Node<T> {
+    is_leaf: bool,
+    is_node: bool,
+
+    chr: char,
+    freq: usize, 
+
+    left: Option,
+    right: Option,
+}
+
+impl Node {
+    pub fn is_leaf(&self) -> bool {
+        return self.is_leaf;
+    }
+
+    pub fn is_node(&self) -> bool {
+        return self.is_node;
+    }
+
+    pub fn new_leaf(&mut self, chr: char, freq: usize) -> Node {
+        self.is_leaf = true;
+        self.chr = chr;
+        self.freq = freq;
+    }
+
+    pub fn new_node(&mut self, l: &Node, r: &Node) -> Node {
+        self.is_node = true;
+        self.chr = cmp::max(l.chr, r.chr);
+        self.freq = l.freq + r.freq;
+    }
+
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Node) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Node) -> Ordering {
+        if self.charcount == other.charcount {
+            if self.is_node {
+                return Ordering::Less;
+            }
+            if other.is_node {
+                return Ordering::Greater;
+            }
+            return self.character.cmp(&other.character);
+        }
+        self.charcount.cmp(other.charcount)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    ;
 }
